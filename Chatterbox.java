7025763812aa96362fb15ece190edfab9a40738c
RakshitaRRao <83875392+RakshitaRRao@users.noreply.github.com>
2021-05-30 19:29:42 +0530

    Create Chatterbox.java

---
+++ Chatterbox.java
@@ -0,0 +1,399 @@
+import java.io.*;
+public class talk_or_DIE
+{
+   public static void main(String args[]) throws IOException, java.lang.InterruptedException
+   {
+       InputStreamReader read= new InputStreamReader(System.in);
+       BufferedReader in= new BufferedReader(read);
+       System.out.println("hey i'm Rakshita!");
+       Thread.sleep(1000);
+       System.out.print("And I wrote this code to steal all your money!");
+       Thread.sleep(500);
+       System.out.print(".");
+       Thread.sleep(500);
+       System.out.print(".");
+       Thread.sleep(500);
+       System.out.print(".");
+       Thread.sleep(500);
+       System.out.print(".");
+       Thread.sleep(500);
+       System.out.print(".");
+       Thread.sleep(500);
+       System.out.print(".");
+       Thread.sleep(500);
+       System.out.println("jkjkjkjkjk ily");
+       Thread.sleep(1000);
+       System.out.println("hey! you never told me your name!!");
+       String n = in.readLine();
+       System.out.println("hey "+n+ "!"+" wanna be my friend?");
+       String a= in.readLine();
+       if(a.equals("yes") || a.equals("sure") || a.equals("yeah") || a.equals("ye") || a.equals("yea"))
+           {
+           System.out.println("cool!");
+           Thread.sleep(1000);
+           System.out.println("first let's get to know each other a little");
+           Thread.sleep(1000);
+           System.out.println("question 1:");
+           Thread.sleep(1000);
+           System.out.print("what ");
+           Thread.sleep(1000);
+           System.out.print("is ");
+           Thread.sleep(1000);
+           System.out.print("your ");
+           Thread.sleep(1000);
+           System.out.println("credit card number");
+           Thread.sleep(5000);
+           System.out.println("jk again!! I really got you good huh");
+           Thread.sleep(1000);
+           System.out.println("yo forgive me i swear this is the last time");
+           Thread.sleep(1000);
+           System.out.println("kk real question 1 now");
+           Thread.sleep(1000);
+           System.out.println("what is your favorite color?");
+           String c= in.readLine();
+           if(c.equalsIgnoreCase("turqouise"))
+           {
+               System.out.println("???????????are we twins?????????????");
+           }
+           else
+           {    
+               System.out.println("omg thats so cooooooooooooool!!!!!!!!!! Mine is turquoise");
+           }
+           Thread.sleep(1000);
+           System.out.println("question 2:");
+           Thread.sleep(1000);
+           System.out.println("what is your favorite sport?");
+           String s= in.readLine();
+           if(s.equalsIgnoreCase("basketball"))
+           {
+               System.out.println("yes");
+           }
+           else
+           {
+               System.out.println("awesome!! my favorite sport is basketball");
+           }
+           Thread.sleep(1000);
+           System.out.println("question 3:");
+           Thread.sleep(1000);
+           System.out.println("what hobby do enjoy?");
+           String h= in.readLine();
+           if(h.equalsIgnoreCase("drawing")||h.equalsIgnoreCase("arts and crafts"))
+           {
+               System.out.println("same!! drawing helps me unwind and relax");
+           }
+           else
+           {
+               System.out.println("that sounds interesting!! personally drawing helps me unwind and relax");
+           }
+           Thread.sleep(1000);
+           System.out.println("question 4:");
+           Thread.sleep(1000);
+           System.out.println("do you have any siblings?");
+           String sib= in.readLine();
+           if(sib.equalsIgnoreCase("yes")||sib.equalsIgnoreCase("yup"))
+           {
+               System.out.println("cool!! how many?");
+               int sib2= Integer.parseInt(in.readLine());
+               System.out.println("daaaw! They must be so cute!! I have one myself and shes so sweet(even tho she *caaan* be annoying sometimes)");
+           }
+           else
+           {
+               System.out.println("aww!! I'm sure if you had one you would love 'em like i love my sister");
+           }
+
+
+Thread.sleep(1000);
+           System.out.println("question 5:");
+           Thread.sleep(1000);
+           System.out.println("do you watch cartoons?");
+           String cart= in.readLine();
+           if(cart.equalsIgnoreCase("yes")||sib.equalsIgnoreCase("yup") || cart.equalsIgnoreCase("yea"))
+           {
+               System.out.println("awesome!! my favorite is Miraculous: tales of ladybug and chat noir");
+               Thread.sleep(1000);
+               System.out.println("do you have a favorite?");
+               String c2= in.readLine();
+               if(c2.equalsIgnoreCase("yes"))
+               {
+                   System.out.println("awesome!! Which one?");
+                   String c3= in.readLine();
+                   if(c3.equalsIgnoreCase("miraculous:tales of ladybug and chat noir") || c3.equalsIgnoreCase("miraculous") || c3.equalsIgnoreCase("miraculous ladybug"))
+                   {
+                       System.out.println("bestie bestie bESTIE BESTIE SAAAAAAAAAAAAAAAAAAME");
+                   }
+                   else
+                   {
+                       System.out.println("oh! sounds cool!!");
+                   }
+               }
+           }
+           else
+           {
+               System.out.println("that's cool too!! ");
+           }
+           Thread.sleep(1000);
+           System.out.println("question 6:");
+           Thread.sleep(1000);
+           System.out.println("what is your favorite food?");
+           String f= in.readLine();
+           Thread.sleep(1000);
+           System.out.println("ooooh thats cool! i dont really have a *favorite*");
+           Thread.sleep(1000);
+           System.out.println("question 7:");
+           Thread.sleep(1000);
+           System.out.println("do you have any pets?");
+           String p= in.readLine();
+           if(p.equalsIgnoreCase("yes")||p.equalsIgnoreCase("yup"))
+           {
+               System.out.println("cool!! I dont have one but i've always wanted a dog! :')");
+           }
+           else
+           {
+               System.out.println("me neither!! But ive always wanted a dog! :')");
+           }
+           Thread.sleep(1000);
+           System.out.println("question 8:");
+           Thread.sleep(1000);
+           System.out.println("have you read any novels (graphic novels count too!!)?");
+           String b= in.readLine();
+           if(b.equalsIgnoreCase("yes")||b.equalsIgnoreCase("yup"))
+           {
+               System.out.println("awesome!! my favorite(s) have to the percy Jackson series(not the excuse of a movie) and harry potter!!");
+           }
+           else
+           {
+               System.out.println("that's good too!!");
+           }
+           Thread.sleep(1000);
+           System.out.println("question 9:");
+           Thread.sleep(1000);
+           System.out.println("do you have a favorite song?");
+           String s2= in.readLine();
+           if(s2.equalsIgnoreCase("yes")||s2.equalsIgnoreCase("yup"))
+           {
+               System.out.println("cool!!which one??");
+               String ws= in.readLine();
+               System.out.println("oh sounds good!! maybe I'll listen to it later :P");
+               Thread.sleep(1000);
+               System.out.println("my favorite song is *ʷʰᶦˢᵖᵉʳˢ* the hawkmoth rap");
+           }
+           else
+           {
+               System.out.println("thats alright!!");
+           }
+           System.out.println("question 10:");
+           Thread.sleep(1000);
+           System.out.println("last one");
+           Thread.sleep(1000);
+           System.out.println("the reaaaallllyyy important one");
+           Thread.sleep(1000);
+           System.out.print("what ");
+           Thread.sleep(1000);
+           System.out.print("do ");
+           Thread.sleep(1000);
+           System.out.print("you ");
+           Thread.sleep(1000);
+           System.out.print("think ");
+           Thread.sleep(1000);
+           System.out.print("about ");
+           Thread.sleep(1000);
+           System.out.println(" memes?");
+           String m= in.readLine();
+           if(m.equalsIgnoreCase("they're cool")||m.equalsIgnoreCase("cool") || m.equalsIgnoreCase("memes") || m.equalsIgnoreCase("yes"))
+           {
+               System.out.println("thamk you bestie!!");
+           }
+           else
+           {
+               System.out.println("*insert y tho meme here*");
+           }
+        }
+        else if(a.equals("well") || a.equals("i'll think about it"))
+        {
+            System.out.println("weeelllll let me just tell you im a good person");
+            String a2 = in.readLine(); 
+            if(a2.equals("ok") || a2.equals("alright!"))
+            {
+                System.out.println("cool!");
+           Thread.sleep(1000);
+           System.out.println("first let's get to know each other a little");
+           Thread.sleep(1000);
+           System.out.println("question 1:");
+           Thread.sleep(1000);
+           System.out.print("what ");
+           Thread.sleep(1000);
+           System.out.print("is ");
+           Thread.sleep(1000);
+           System.out.print("your ");
+           Thread.sleep(1000);
+           System.out.println("credit card number");
+           Thread.sleep(5000);
+           System.out.println("jk again!! I really got you good huh");
+           Thread.sleep(1000);
+           System.out.println("yo forgive me i swear this is the last time");
+           Thread.sleep(1000);
+           System.out.println("kk real question 1 now");
+           Thread.sleep(1000);
+           System.out.println("what is your favorite color?");
+           String c= in.readLine();
+           if(c.equalsIgnoreCase("turqouise"))
+           {
+               System.out.println("???????????are we twins?????????????");
+           }
+           else
+           {    
+               System.out.println("omg thats so cooooooooooooool!!!!!!!!!! Mine is turquoise");
+           }
+           Thread.sleep(1000);
+           System.out.println("question 2:");
+           Thread.sleep(1000);
+           System.out.println("what is your favorite sport?");
+           String s= in.readLine();
+           if(s.equalsIgnoreCase("basketball"))
+           {
+               System.out.println("yes");
+           }
+           else
+           {
+               System.out.println("awesome!! my favorite sport is basketball");
+           }
+           Thread.sleep(1000);
+           System.out.println("question 3:");
+           Thread.sleep(1000);
+           System.out.println("what hobby do enjoy?");
+           String h= in.readLine();
+           if(h.equalsIgnoreCase("drawing")||h.equalsIgnoreCase("arts and crafts"))
+           {
+               System.out.println("same!! drawing helps me unwind and relax");
+           }
+           else
+           {
+               System.out.println("that sounds interesting!! personally drawing helps me unwind and relax");
+           }
+           Thread.sleep(1000);
+           System.out.println("question 4:");
+           Thread.sleep(1000);
+           System.out.println("do you have any siblings?");
+           String sib= in.readLine();
+           if(sib.equalsIgnoreCase("yes")||sib.equalsIgnoreCase("yup"))
+           {
+               System.out.println("cool!! how many?");
+               System.out.println("daaaw! They must be so cute!! I have one myself and shes so sweet(even tho she *caaan* be annoying sometimes)");
+           }
+           else
+           {
+               System.out.println("aww!! I'm sure if you had one you would love 'em like i love my sister");
+           }
+           Thread.sleep(1000);
+           System.out.println("question 5:");
+           Thread.sleep(1000);
+           System.out.println("do you watch cartoons?");
+           String cart= in.readLine();
+           if(cart.equalsIgnoreCase("yes")||sib.equalsIgnoreCase("yup") || cart.equalsIgnoreCase("yea"))
+           {
+               System.out.println("awesome!! my favorite is Miraculous: tales of ladybug and chat noir");
+               Thread.sleep(1000);
+               System.out.println("do you have a favorite?");
+                String c2= in.readLine();
+               if(c2.equalsIgnoreCase("yes"))
+               {
+                   System.out.println("awesome!! Which one?");
+                   String c3= in.readLine();
+                   if(c3.equalsIgnoreCase("miraculous:tales of ladybug and chat noir") || c3.equalsIgnoreCase("miraculous") || c3.equalsIgnoreCase("miraculous ladybug"))
+                   {
+                       System.out.println("bestie bestie bESTIE BESTIE SAAAAAAAAAAAAAAAAAAME");
+                   }
+                   else
+                   {
+                       System.out.println("oh! sounds cool!!");
+                   }
+               }
+           }
+           else
+           {
+               System.out.println("that's cool too!! ");
+           }
+           Thread.sleep(1000);
+           System.out.println("question 6:");
+           Thread.sleep(1000);
+           System.out.println("what is your favorite food?");
+           String f= in.readLine();
+           Thread.sleep(1000);
+           System.out.println("ooooh thats cool! i dont really have a *favorite*");
+           Thread.sleep(1000);
+           System.out.println("question 7:");
+           Thread.sleep(1000);
+           System.out.println("do you have any pets?");
+           String p= in.readLine();
+           if(p.equalsIgnoreCase("yes")||p.equalsIgnoreCase("yup"))
+           {
+               System.out.println("cool!! I dont have one but i've always wanted a dog! :')");
+           }
+           else
+           {
+               System.out.println("me neither!! But ive always wanted a dog! :')");
+           }
+           Thread.sleep(1000);
+           System.out.println("question 8:");
+           Thread.sleep(1000);
+           System.out.println("have you read any novels (graphic novels count too!!)?");
+           String b= in.readLine();
+           if(b.equalsIgnoreCase("yes")||b.equalsIgnoreCase("yup"))
+           {
+               System.out.println("awesome!! my favorite(s) have to the percy Jackson series(not the excuse of a movie) and harry potter!!");
+           }
+           else
+           {
+               System.out.println("that's good too!!");
+           }
+           Thread.sleep(1000);
+           System.out.println("question 9:");
+           Thread.sleep(1000);
+           System.out.println("do you have a favorite song?");
+           String s2= in.readLine();
+           if(s2.equalsIgnoreCase("yes")||s2.equalsIgnoreCase("yup"))
+           {
+               System.out.println("cool!!which one??");
+               String ws= in.readLine();
+               System.out.println("oh sounds good!! maybe I'll listen to it later :P");
+               Thread.sleep(1000);
+               System.out.println("my favorite song is *ʷʰᶦˢᵖᵉʳˢ* the hawkmoth rap");
+           }
+           else
+           {
+               System.out.println("thats alright!!");
+           }
+           System.out.println("question 10:");
+           Thread.sleep(1000);
+           System.out.println("last one");
+           Thread.sleep(1000);
+           System.out.println("the reaaaallllyyy important one");
+           Thread.sleep(1000);
+           System.out.print("what ");
+           Thread.sleep(1000);
+           System.out.print("do ");
+           Thread.sleep(1000);
+           System.out.print("you ");
+           Thread.sleep(1000);
+           System.out.print("think ");
+           Thread.sleep(1000);
+           System.out.print("about ");
+           Thread.sleep(1000);
+           System.out.println(" memes?");
+           String m= in.readLine();
+           if(m.equalsIgnoreCase("they're cool")||m.equalsIgnoreCase("cool") || m.equalsIgnoreCase("memes") || m.equalsIgnoreCase("yes"))
+           {
+               System.out.println("thamk you bestie!!");
+           }
+           else
+           {
+               System.out.println("*insert y tho meme here*");
+           }
+        }
+    }
+        else
+        {
+            System.out.println("aaawwww we could have been good friends");
+   }
+}
+}
